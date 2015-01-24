935cc012967e0ccaaf72839bc13b32321d539d27
Tim Rodger <tim.rodger@gmail.com>
2015-01-24 15:27:12 +0000

    Adds fig config, update client to connect to containerised app

--- public/client.js
+++ public/client.js
@@ -24,7 +24,7 @@ $(function () {
     }
 
     // open connection - parameterize the endpoint
-    var connection = new WebSocket('ws://127.0.0.1:1337');
+    var connection = new WebSocket('ws://192.168.59.103:1337');
 
     connection.onopen = function () {
         // first we want users to enter their names
